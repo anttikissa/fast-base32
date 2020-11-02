5e63e5b9b20002d1e320f54de5be46deb6677018
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2020-11-02 21:38:33 +0200

    Naive BigInt encoder

--- encode.js
+++ encode.js
@@ -1,4 +1,24 @@
-// encode('hello world') => ...
+let alphabet = '0123456789abcdefghjkmnpqrstvwxyz'
+
 export function encode(buffer) {
-	return '(encode me) ' + buffer.toString('hex')
+	if (typeof buffer === 'string') {
+		buffer = Buffer.from(buffer)
+	}
+	let bits = buffer.length * 8
+	let n = BigInt('0x' + (buffer.toString('hex') || 0))
+
+	if (n !== 0) {
+		while (bits % 5) {
+			n <<= 1n
+			bits++
+		}
+	}
+
+	let result = ''
+	while (bits > 0) {
+		result = alphabet[n & 31n] + result
+		n >>= 5n
+		bits -= 5
+	}
+	return result
 }
