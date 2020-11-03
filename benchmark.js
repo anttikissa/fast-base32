519a147626485c3534bbbb67dfb6d5a87c5bb918
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2020-11-03 06:00:46 +0200

    Add bogus decode
    encode x 1,149,338 ops/sec ±2.63% (82 runs sampled)
    base32-encode x 1,003,766 ops/sec ±2.83% (86 runs sampled)
    decode x 3,478,563 ops/sec ±2.86% (85 runs sampled)
    base32-decode x 409,863 ops/sec ±2.23% (81 runs sampled)
    encode string x 739,578 ops/sec ±3.03% (84 runs sampled)
    base32-encode string x 790,291 ops/sec ±2.91% (85 runs sampled)

--- benchmark.js
+++ benchmark.js
@@ -1,6 +1,7 @@
 import Benchmark from 'benchmark'
 import { encode, decode } from './index.js'
 import base32encode from 'base32-encode'
+import base32decode from 'base32-decode'
 
 let suite = new Benchmark.Suite
 
@@ -8,25 +9,34 @@ let STR = 'The quick brown fox jumps over the lazy dog.'
 let BUF = Buffer.from(STR)
 let RESULT = encode(STR)
 
-suite.add('encode Buffer', () => {
-	encode(BUF)
-})
+console.log('result', RESULT)
 
-suite.add('encode string', () => {
-	encode(STR)
+suite.add('encode', () => {
+	encode(BUF)
 })
 
 suite.add('base32-encode', () => {
 	base32encode(BUF, 'Crockford')
 })
 
+suite.add('decode', () => {
+	decode(RESULT)
+})
+
+suite.add('base32-decode', () => {
+	base32decode(RESULT, 'Crockford')
+})
+
+suite.add('encode string', () => {
+	encode(STR)
+})
+
+
+
 suite.add('base32-encode string', () => {
 	base32encode(Buffer.from(STR), 'Crockford')
 })
 
-// suite.add('decode', () => {
-// 	decode(RESULT)
-// })
 
 
 
--- decode.js
+++ decode.js
@@ -1,3 +1,14 @@
 export function decode(encoded) {
-	return 'TODO'
+	let length = Math.ceil(encoded.length * 5 / 8)
+	let result = Buffer.alloc(length)
+
+	// Just produce something stupid to see what's the performance cap
+	for (let i = 0; i < length; i++) {
+		result[i] = encoded.charCodeAt(i)
+	}
+
+	return result
 }
+
+console.log(decode('ahm6a83henmp6ts0c9s6yxve41k6yy10d9tptw3k41qqcsbj41t6gs90dhgqmy90chqpebg'))
+
