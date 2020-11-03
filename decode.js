5e280ddfb3120fb45133d9ae544afb5bbfb2917a
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2020-11-03 06:31:02 +0200

    Start to approximate the final decoding algorithm

--- decode.js
+++ decode.js
@@ -1,4 +1,15 @@
+let alphabet = '0123456789abcdefghjkmnpqrstvwxyz'
+let reverseAlphabet = Array(256)
+for (let i = 0; i < alphabet.length; i++) {
+	reverseAlphabet[alphabet.charCodeAt(i)] = i
+}
 
+/**
+ * Base32-decode 'input'
+ *
+ * @param {string} input
+ * @returns {Buffer}
+ */
 export function decode(input) {
 	let inputLength = input.length
 	let length = Math.floor(input.length * 5 / 8)
@@ -6,14 +17,17 @@ export function decode(input) {
 
 	let bits = 0
 	let value = 0
+	let resultPos = 0
 
 	for (let i = 0; i < inputLength; i++) {
 		bits += 5
-		input.charCodeAt
-	}
 
-	for (let i = 0; i < length; i++) {
-		result[i] = input.charCodeAt(i)
+		value = value << 5 + reverseAlphabet[input.charCodeAt(i)]
+
+		while (bits >= 8) {
+			result[resultPos++] = 0x88
+			bits -= 8
+		}
 	}
 
 	return result
