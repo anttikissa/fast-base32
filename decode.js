576ab2e6a54996ed882f690b3a98945e5fee6787
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2020-11-12 23:40:58 +0200

    decode.js: prettier

--- decode.js
+++ decode.js
@@ -1,136 +1,140 @@
 const SKIP = -1
 
 let defaultOptions = {
-    alphabet: '0123456789abcdefghjkmnpqrstvwxyz',
-    sanitize: function (input) {
-        return input.replace(/[A-Zilo*~$=uU-]/g, (c) => {
-            switch (c) {
-                // Ignore hyphens are check symbols
-                case '-':
-                case '*':
-                case '~':
-                case '$':
-                case '=':
-                case 'u':
-                case 'U':
-                    return ''
-                // Error correction
-                case 'o':
-                case 'O':
-                    return '0'
-                case 'i':
-                case 'I':
-                case 'l':
-                case 'L':
-                    return '1'
+	alphabet: '0123456789abcdefghjkmnpqrstvwxyz',
+	sanitize: function (input) {
+		return input.replace(/[A-Zilo*~$=uU-]/g, (c) => {
+			switch (c) {
+				// Ignore hyphens are check symbols
+				case '-':
+				case '*':
+				case '~':
+				case '$':
+				case '=':
+				case 'u':
+				case 'U':
+					return ''
+				// Error correction
+				case 'o':
+				case 'O':
+					return '0'
+				case 'i':
+				case 'I':
+				case 'l':
+				case 'L':
+					return '1'
 
-                // The rest are transformed to lower case
-                default:
-                    return c.toLowerCase()
-            }
-        })
-    },
-    verifyInput: false,
-    verify: function (input) {
-        if (!input.match(/^[0-9a-zA-Z*~$=]*$/)) {
-            throw new Error(`Invalid input: "${input}"`)
-        }
-    }
+				// The rest are transformed to lower case
+				default:
+					return c.toLowerCase()
+			}
+		})
+	},
+	verifyInput: false,
+	verify: function (input) {
+		if (!input.match(/^[0-9a-zA-Z*~$=]*$/)) {
+			throw new Error(`Invalid input: "${input}"`)
+		}
+	},
 }
 
 function getReverseAlphabet({ alphabet, sanitize }) {
-    let result = Array(256)
+	let result = Array(256)
 
-    // By default, map all characters to their indexes in alphabet.
-    // If they don't exist in the alphabet, map them to 0.
-    for (let i = 0; i < 256; i++) {
-        let idx = alphabet.indexOf(String.fromCharCode(i))
-        result[i] = idx !== -1 ? idx : 0
-    }
+	// By default, map all characters to their indexes in alphabet.
+	// If they don't exist in the alphabet, map them to 0.
+	for (let i = 0; i < 256; i++) {
+		let idx = alphabet.indexOf(String.fromCharCode(i))
+		result[i] = idx !== -1 ? idx : 0
+	}
 
-    // Then patch the reverse alphabet so that for every character c, c maps to the
-    // same value as sanitize(c). While at it, check the sanity of the sanitizer.
-    for (let i = 0; i < 128; i++) {
-        let char = String.fromCharCode(i)
-        let sanitizedChar = sanitize(char)
-        if (alphabet.indexOf(char) !== -1) {
-            if (sanitizedChar !== char) {
-                throw new Error(`sanitize mapped valid input '${char}' to '${sanitizedChar}'`)
-            }
-        }
+	// Then patch the reverse alphabet so that for every character c, c maps to the
+	// same value as sanitize(c). While at it, check the sanity of the sanitizer.
+	for (let i = 0; i < 128; i++) {
+		let char = String.fromCharCode(i)
+		let sanitizedChar = sanitize(char)
+		if (alphabet.indexOf(char) !== -1) {
+			if (sanitizedChar !== char) {
+				throw new Error(
+					`sanitize mapped valid input '${char}' to '${sanitizedChar}'`
+				)
+			}
+		}
 
-        if (sanitizedChar === '') {
-            result[i] = SKIP
-        } else {
-            let sanitizedCharCode = sanitizedChar.charCodeAt(0)
+		if (sanitizedChar === '') {
+			result[i] = SKIP
+		} else {
+			let sanitizedCharCode = sanitizedChar.charCodeAt(0)
 
-            if (sanitizedCharCode < 0 || sanitizedCharCode >= 256) {
-                throw new Error(`sanitize('${char}' (${i})) returned an invalid character: ` +
-                    `'${sanitizedChar}' (${sanitizedCharCode})`)
-            }
-            result[i] = result[sanitizedCharCode]
-        }
-    }
+			if (sanitizedCharCode < 0 || sanitizedCharCode >= 256) {
+				throw new Error(
+					`sanitize('${char}' (${i})) returned an invalid character: ` +
+						`'${sanitizedChar}' (${sanitizedCharCode})`
+				)
+			}
+			result[i] = result[sanitizedCharCode]
+		}
+	}
 
-    return result
+	return result
 }
 
 function configure(options) {
-    options = { ...defaultOptions, ...options }
+	options = { ...defaultOptions, ...options }
 
-    const { verifyInput, verify } = options
-    const reverseAlphabet = getReverseAlphabet(options)
+	const { verifyInput, verify } = options
+	const reverseAlphabet = getReverseAlphabet(options)
 
-    /**
-     * Base32-decode 'input'
-     *
-     * @param {string} input
-     * @returns {Buffer}
-     */
-    function decode(input) {
-        if (verifyInput) {
-            verify(input)
-        }
+	/**
+	 * Base32-decode 'input'
+	 *
+	 * @param {string} input
+	 * @returns {Buffer}
+	 */
+	function decode(input) {
+		if (verifyInput) {
+			verify(input)
+		}
 
-        let inputLength = input.length
-        let length = Math.floor((input.length * 5) / 8)
-        let output = Buffer.alloc(length)
+		let inputLength = input.length
+		let length = Math.floor((input.length * 5) / 8)
+		let output = Buffer.alloc(length)
 
-        let bits = 0
-        let value = 0
-        let resultPos = 0
+		let bits = 0
+		let value = 0
+		let resultPos = 0
 
-        let skipped = 0
+		let skipped = 0
 
-        for (let i = 0; i < inputLength; i++) {
-            let undecoded = input.charCodeAt(i)
+		for (let i = 0; i < inputLength; i++) {
+			let undecoded = input.charCodeAt(i)
 
-            let decoded = reverseAlphabet[undecoded]
-            if (decoded === SKIP) {
-                skipped++
-                continue
-            }
-            bits += 5
+			let decoded = reverseAlphabet[undecoded]
+			if (decoded === SKIP) {
+				skipped++
+				continue
+			}
+			bits += 5
 
-            value = (value << 5) + decoded
+			value = (value << 5) + decoded
 
-            while (bits >= 8) {
-                output[resultPos++] = (value >>> (bits - 8)) & 0xff
-                bits -= 8
-            }
-        }
+			while (bits >= 8) {
+				output[resultPos++] = (value >>> (bits - 8)) & 0xff
+				bits -= 8
+			}
+		}
 
-        if (skipped) {
-            let actualLength = Math.floor(((input.length - skipped) * 5) / 8)
-            output = output.slice(0, actualLength)
-        }
+		if (skipped) {
+			let actualLength = Math.floor(((input.length - skipped) * 5) / 8)
+			output = output.slice(0, actualLength)
+		}
 
-        return output
-    }
+		return output
+	}
 
-    decode.configure = configure
-    decode.options = options
-    return decode
+	decode.configure = configure
+	decode.options = options
+	return decode
 }
 
 export const decode = configure(defaultOptions)
